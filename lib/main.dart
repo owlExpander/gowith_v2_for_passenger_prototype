5b6f92873d04e63b0a444bde8b96f42a21c3b56d
Jong-hyun Park <jhpark@mtis21.com>
2023-01-20 17:10:30 +0900

    카카오네비 연동 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-//import 'package:location/location.dart';
+//import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
+import 'package:kakao_flutter_sdk_navi/kakao_flutter_sdk_navi.dart';
 
 import 'package:gowith_v2_for_passenger_prototype/page_ticket.dart';
 import 'package:gowith_v2_for_passenger_prototype/page_route.dart';
@@ -12,6 +13,14 @@ import 'package:gowith_v2_for_passenger_prototype/util_location.dart';
 const String appTitle = '동행v2 (승객용) v0.4';
 
 void main() {
+  // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // runApp() 호출 전 Flutter SDK 초기화
+  KakaoSdk.init(
+      nativeAppKey: 'b02c980b8d9caf1c7d24ef9547fb4c39',
+  );
+
   runApp(const MyApp());
 }
 
@@ -78,8 +87,40 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           title: const Text(appTitle),
           actions: [
+            // 네비게이션 실행 테스트
+            IconButton(
+                onPressed: () async {
+                  //print(await KakaoSdk.origin);
+
+                  if (await NaviApi.instance.isKakaoNaviInstalled()) {
+                    // 카카오내비 앱으로 길 안내하기, WGS84 좌표계 사용
+                    await NaviApi.instance.navigate(
+                      option: NaviOption(
+                          coordType: CoordType.wgs84,
+                          vehicleType: VehicleType.third,
+                      ),
+                      destination: Location(name: '어바니엘가산', x: '126.87976185037579', y: '37.4849314292158'),
+                      // 경유지 추가
+                      viaList: [
+                        Location(name: '한화 솔루션큐셀', x: '126.88251119816695', y: '37.476545805363216'),
+                        Location(name: '서울 가산디지털 우체국', x: '126.88020295443398', y: '37.47775984994049'),
+                        Location(name: '카페 마중물', x: '126.8788983048132', y: '37.48055164721279'),
+                        Location(name: '에이스 K1타워', x: '126.87803582657943', y: '37.48253297857724'),
+                        Location(name: '리츠빌 아파트', x: '126.87761051212924', y: '37.486812326444735'),
+                      ],
+                    );
+                  } else {
+                    //print('카카오내비 미설치');
+                    // 카카오내비 설치 페이지로 이동
+                    launchBrowserTab(Uri.parse(NaviApi.webNaviInstall));
+                  }
+                },
+                icon: const Icon(Icons.navigation_outlined)
+            ),
             // QR 버튼
             IconButton(
+                visualDensity: const VisualDensity(horizontal: -4.0, vertical: -4.0), // 아이콘 간격 줄임
+                padding: EdgeInsets.zero,                                             // 아이콘 간격 줄임
                 onPressed: () {
                   showDialog<void>(context: context, builder: (context) => _showAlertDialog('준비중 입니다.'));
                 },
@@ -96,6 +137,8 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             // 메뉴 버튼
             IconButton(
+                visualDensity: const VisualDensity(horizontal: -4.0, vertical: -4.0), // 아이콘 간격 줄임
+                padding: EdgeInsets.zero,                                             // 아이콘 간격 줄임
                 onPressed: _openEndDrawer,
                 icon: const Icon(Icons.menu)
             ),
